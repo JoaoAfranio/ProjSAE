ca7388b93f89c3a3a0becc11fa25055518e13376
João Afrânio <joaoafraniorosa@hotmail.com>
2019-04-24 19:26:33 -0300

    Organizando e Refazendo CRUD (PDO)

---
+++ template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>SAE</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../css/iconfonts/mdi/font/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="../css/vendor.bundle.addons.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <link rel="stylesheet" href="http://www.urbanui.com/">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../images/favicon.ico" />
+</head>
